d7a8ef2f732796d51c38ac9244f7317f463d5dcb
Alfarhan Zahedi <alfarhanzahedi@gmail.com>
2017-12-09 23:59:49 +0530

    updated script
    resolved "index out of bound error" and extended support for scraping
    phone numbers.

--- jdscraper.py
+++ jdscraper.py
@@ -1,16 +1,14 @@
 import bs4, requests, sys, csv, pyperclip
-base_url = sys.argv[1] or pyperclip.paste()
+base_url = sys.argv[1] if len(sys.argv) > 2 else pyperclip.paste()
 arr = []
-info = []
-file = open('info.txt', 'w')
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36', 'cookie':'inweb_city=Kolkata;'}
 res = requests.get(base_url, headers=headers)
 res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text, "html.parser")
 elems = soup.select("li['data-href']")
 title = soup.select('.input-control')[0].get('value')
-mycsvfile = open(title + '.csv','a')
-csv.writer(mycsvfile).writerow(['Name','Address','Contact','Rating'])
+mycsvfile = open(title + '.csv','w')
+csv.writer(mycsvfile).writerow(['Name','Address','Contact-1', 'Contact-2', 'Rating'])
 counter = 2
 while len(elems) > 0:
     for j in range(len(elems)):
@@ -18,15 +16,64 @@ while len(elems) > 0:
         res = requests.get(url, headers=headers)
         res.raise_for_status()
         soup = bs4.BeautifulSoup(res.text,"html.parser")
-        print("Saving data for: %s" %(soup.select('span[itemprop="name"]')[0].getText()))
-        arr.append(soup.select('span[itemprop="name"]')[0].getText())
-        arr.append(soup.select('span[itemprop="streetAddress"]')[0].getText())
-        rating = soup.select('.value-titles')[0].getText() if len(soup.select('.value-titles')) > 0 else 'NA'
-        if (len(soup.select('.tel'))) != 0:
-            contact = (soup.select('.tel b')[0].getText() if ((len(soup.select('.tel b'))) > 0) else soup.select('.tel')[0].getText())
+        print("Saving data for: %s" %(soup.select('span[class="fn"]')[0].getText()))
+        arr.append(soup.select('span[class="fn"]')[0].getText().title())
+        address = soup.select('span[class="lng_add"]')
+        if len(address) <= 2:
+            arr.append(soup.select('span[class="lng_add"]')[1].getText())
         else:
-            contact = 'NA'
-        arr.append(contact)
+            arr.append(soup.select('span[class="lng_add"]')[2].getText())
+        temp = soup.select('a[class="tel"]')
+        rating = soup.select('.value-titles')[0].getText() if len(soup.select('.value-titles')) > 0 else 'NA'
+        
+        style_tag = soup.select('style')
+        icons = (str(style_tag[1]).split("."))
+        mp = {}
+        for e in icons:
+            if "icon-" in e:
+                if "content" in e:
+                    mp[e.split("{")[0].split(":")[0]] = (int(e.split('"')[1][-2:])) - 1
+
+        mobile_numbers = soup.select('span[class="telnowpr"]')
+        if len(mobile_numbers) > 0:
+            mobile_numbers = mobile_numbers[0];
+            mobile_arr = []
+            for items in mobile_numbers:
+                mobile_arr.append(str(items))
+            res = ''.join(mobile_arr)
+            soup_mobile = bs4.BeautifulSoup(res,"html.parser")
+            temp = soup_mobile.findAll("a", { "class" : "tel" })
+            res_one = str(soup_mobile.findAll("a", { "class" : "tel" })[0])
+            soup_one = bs4.BeautifulSoup(res_one,"html.parser")
+            digits = (soup_one.findAll("span", { "class" : "mobilesv" }))
+            s = []
+            for elem in digits:
+                s.append(str(mp[elem["class"][1]] if mp[elem["class"][1]] != 10 else '+'))
+            phone = ''.join(s)
+            if(''.join(s[0:3]) == '033'):
+                s = s[0:3] + [" "] + s[3:]
+            elif(''.join(s[0:3]) == '+91'):
+                s = s[0:3] + [" "] + s[3:]
+            arr.append(''.join(s))
+
+            if len(temp) > 1:
+                res_two = str(soup_mobile.findAll("a", { "class" : "tel" })[1])
+                soup_two = bs4.BeautifulSoup(res_two,"html.parser")
+                digits = (soup_two.findAll("span", { "class" : "mobilesv" }))
+                s = []
+                for elem in digits:
+                    s.append(str(mp[elem["class"][1]] if mp[elem["class"][1]] != 10 else '+'))
+                phone = ''.join(s)
+                if(''.join(s[0:3]) == '+91'):
+                    s = s[0:3] + [" "] + s[3:]
+                elif(''.join(s[0:3]) == '033'):
+                    s = s[0:3] + [" "] + s[3:]
+                arr.append(''.join(s))
+            else:
+                arr.append('NA')
+        else:
+            arr.append('NA')
+            arr.append('NA')
         arr.append(rating)
         csv.writer(mycsvfile).writerow(arr)
         arr = []
@@ -35,7 +82,3 @@ while len(elems) > 0:
     soup = bs4.BeautifulSoup(res.text, "html.parser")
     elems = soup.select("li['data-href']")
     counter += 1
-
-        
-    
-    
